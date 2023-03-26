787706deb126507338b630fefc02edcfe3043edf
Susana Ribeiro <118482943+SusananRibeiro@users.noreply.github.com>
2023-03-26 18:36:34 -0300

    first commit

---
+++ src/main/java/com/ultimaschool/pessoas/PessoaIF.java
@@ -0,0 +1,5 @@
+package com.ultimaschool.pessoas;
+public interface PessoaIF {
+    public abstract String verSituacao(int mes); // termina com “;”, porque não vai ser implementado
+    public abstract String relatorio(); // termina com “;”, porque não vai ser implementado
+}
