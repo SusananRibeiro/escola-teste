a3ed68ab1c6452a12f9319743aa98041877e5a9d
Susana Ribeiro <118482943+SusananRibeiro@users.noreply.github.com>
2023-03-27 09:48:20 -0300

    Adicionando testes ao projeto

--- src/main/java/com/ultimaschool/pessoas/Responsavel.java
+++ src/main/java/com/ultimaschool/pessoas/Responsavel.java
@@ -1,27 +1,47 @@
 package com.ultimaschool.pessoas;
+import java.util.ArrayList;
 public class Responsavel extends Pessoa {
-    private boolean[] pagamentos = new boolean[12];
+    final int MESES = 12;
+    private boolean[] pagamentos = new boolean[MESES];
     public Responsavel(String nome) {
         super(nome);
     }
     public boolean[] getPagamentos() {
-        return pagamentos;
+        return this.pagamentos;
     }
     public void setPagamentos(boolean[] pagamentos) {
+
         this.pagamentos = pagamentos;
     }
     public boolean pagarMensalidade(int mes, boolean situacao) {
-        // TODO implementar
-        return false;
+        pagamentos[mes] = situacao;
+        return situacao;
     }
     @Override
     public String verSituacao(int mes) {
-        // TODO implementar
-        return null;
+        if(getPagamentos()[mes]) {
+            return "O pagamento foi realizado no mês " + mes;
+        }
+        return "O pagamento não foi realizado no mês " + mes;
+    }
+    private String converteListaEmString(ArrayList<Integer> lista) {
+        String resultado = "";
+        for(int l : lista) {
+            resultado += l + ", ";
+        }
+        return resultado;
+    }
+    private String mesesComPagamento(){
+        ArrayList<Integer> listaMesesPagos = new ArrayList<Integer>();
+        for(int mes = 1; mes < pagamentos.length; mes++){
+            if(pagamentos[mes]){
+                listaMesesPagos.add(mes);
+            }
+        }
+        return "Os meses com pagamento foram: " + converteListaEmString(listaMesesPagos);
     }
     @Override
     public String relatorio() {
-        // TODO implementar
-        return null;
+        return "O responsável financeiro " + getNome() + " realizou o pagamento " + mesesComPagamento();
     }
 }
---
+++ src/test/java/com/ultimaschool/pessoas/EstudanteTest.java
@@ -0,0 +1,46 @@
+package com.ultimaschool.pessoas;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EstudanteTest {
+    Estudante estudante1 = new Estudante("Susana");
+    Estudante estudante2 = new Estudante("Susana");
+    Estudante estudante3 = new Estudante("Susana");
+    @BeforeEach
+    void setUp() {
+    }
+    @Test
+    void testGetMatricula() {
+    }
+    @Test
+    void testSetMatricula() {
+    }
+    @Test
+    void testGetNotas() {
+    }
+    @Test
+    void testSetNotas() {
+    }
+    @Test
+    void testConsolidarNota() {
+    }
+    @Test
+    void testCalcularMediaFinal() {
+    }
+    @Test
+    void testVerSituacao() {
+    }
+    @Test
+    void testRelatorio() {
+    }
+    @AfterEach
+    void tearDown() {
+        estudante1 = null;
+        estudante2 = null;
+        estudante3 = null;
+    }
+}
